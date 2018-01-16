6d797e5ea19413c208779f35503239d4e1256c80
aubucuo <aubucuo@foxmail.com>
2018-01-16 17:23:37 +0800

    to one file

--- tinipic.py
+++ tinipic.py
@@ -5,10 +5,26 @@ Created on Mon Jan 15 08:12:50 2018
 @author: Administrator
 """
 import tinify , os
-from qiniuupload import _uploadpic
+from qiniu import Auth, put_file, etag
 
-tinify.key = "key" #API KEY
+tinify.key = "替换"
 
+#需要填写你的 Access Key 和 Secret Key
+access_key = '替换'
+secret_key = '替换'
+#构建鉴权对象
+q = Auth(access_key, secret_key)
+#要上传的空间
+bucket_name = '替换'
+    
+def _uploadpic(filepath,key):
+    #key是上传之后的文件名
+    #生成上传 Token，可以指定过期时间等
+    token = q.upload_token(bucket_name, key, 3600)
+    ret, info = put_file(token, key, filepath)
+#    print(info)
+    assert ret['key'] == key
+    assert ret['hash'] == etag(filepath)
 def _getpic(path): #获得文件夹pic
     fullname = []
     filename = []
@@ -27,14 +43,14 @@ def _getpic(path): #获得文件夹pic
         #将这张压缩后的图片上传到七牛云
         print ("上传到七牛云！")
         _uploadpic(savepath,name)
-        piclink.append("自己的七牛域名"+name)
+        piclink.append("替换"+name) #七牛云外链
     #写入文件外链
-    with open('F:/tinipic/aalink.txt','a') as f: #加个aa 可以显示在文件夹前面，容易看到
+    with open('F:/tinipic/aalink.txt','a') as f:
         n = 0
         for i in piclink:
-            f.write(i)
+            f.write('\n'+i)
             n += 1
-        f.write("本次%d张图片.END"%n)
+        f.write("\n本次%d张图片.END"%n)
         f.close()
         
 def _tinipic(largepic,tinipic):
@@ -50,4 +66,4 @@ if __name__ == '__main__':
     path = 'F:\largepic'
     _getpic(path)
     print ('完成！')
-
+   
