262cfd7a83020b7979dc1150840c802e50762b54
selfcomputer <aubucuo@qq.com>
2018-10-17 22:08:55 +0800

    链表中倒数第k个节点

---
+++ nowcoder/14_链表中倒数第k个节点.py
@@ -0,0 +1,13 @@
+#coding:u8
+#输入一个链表，输出该链表中倒数第k个结点。
+
+class Solution:
+    def FindKthToTail(self, head, k):
+        # write code here
+        l=[]
+        while head!=None:
+            l.append(head)
+            head=head.next
+        if k>len(l) or k<1:
+            return
+        return l[-k]
